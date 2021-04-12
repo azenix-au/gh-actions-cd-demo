b5f8a09049549c441659d61a45addb3629b69737
Adrian Lee <kleeadrian@github.com>
2021-04-12 22:30:22 +1000

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstatefile"
+    storage_account_name = "tfstatefileacc"
+    container_name       = "tfstated"
+    key                  = "example.tfstate"
+  }
+}
+
+provider "azurerm" {
+  # The "feature" block is required for AzureRM provider 2.x.
+  # If you're using version 1.x, the "features" block is not allowed.
+  version = "~>2.0"
+  features {}
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "rsg_app" {
+  name     = "app-service-example"
+  location = "Australia East"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "app-service-example-s1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
