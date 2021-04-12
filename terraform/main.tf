cba1dc1d519df9b0f32942eb70835956092b6133
Adrian Lee <kleeadrian@github.com>
2021-04-12 23:11:39 +1000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,8 +23,8 @@ resource "azurerm_resource_group" "rsg_app" {
 
 resource "azurerm_app_service_plan" "example" {
   name                = "app-service-example-s1"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.rsg_app.location
+  resource_group_name = azurerm_resource_group.rsg_app.name
   kind                = "Linux"
   reserved            = true
 
