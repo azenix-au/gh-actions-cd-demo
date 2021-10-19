c6b2321ba69aedfa863268e797675bb856f4932f
Peter Hall <peter.hall@digio.com.au>
2021-10-20 09:53:38 +1100

    fixing tf azure versioning

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,12 +5,15 @@ terraform {
     container_name       = "tfstate"
     key                  = "example.tfstate"
   }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
 }
 
 provider "azurerm" {
-  # The "feature" block is required for AzureRM provider 2.x.
-  # If you're using version 1.x, the "features" block is not allowed.
-  version = "~>2.0"
   features {}
 }
 
