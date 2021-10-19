c45a99604641f5f3a45a0b1e9162a7397a248e0e
Peter Hall <ph@ll.id.au>
2021-10-19 13:33:47 +1100

    updating tf state

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tfstatefile"
-    storage_account_name = "tfstatefileacc"
+    resource_group_name  = "phall-github-actions"
+    storage_account_name = "phalltfstatefile"
     container_name       = "tfstate"
     key                  = "example.tfstate"
   }
